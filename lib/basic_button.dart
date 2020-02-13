35d8152bc7a676c5fe62956fab75d1fb524f702a
thevikke <31620078+thevikke@users.noreply.github.com>
2020-02-13 19:04:43 +0200

    Start Page
    Added characters and the wall to the start page, also added cool new font.

---
+++ lib/basic_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class BasicButton extends StatelessWidget {
+  Function onPressed;
+  final String text;
+  final Color color;
+  final Color textColor;
+  BasicButton(
+    this.onPressed, {
+    this.text = " ",
+    this.color = Colors.blue,
+    this.textColor = Colors.white,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: Text(
+        text,
+        style: TextStyle(
+          color: textColor,
+          fontSize: 28,
+          fontFamily: "Frijole",
+        ),
+      ),
+      color: color,
+      onPressed: onPressed,
+    );
+  }
+}
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,12 +1,131 @@
+import 'package:fgj_2020/basic_button.dart';
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_circular_text/circular_text.dart';
+import 'package:nima/nima_actor.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({Key key}) : super(key: key);
+  StartScreen({Key key}) : super(key: key);
 
+  final Image imageStoneWallBlock = Image.asset(
+    "assets/StoneBlock.png",
+    color: Colors.brown,
+    colorBlendMode: BlendMode.softLight,
+  );
   @override
   Widget build(BuildContext context) {
+    final _size = MediaQuery.of(context).size;
     return Container(
       color: Colors.orangeAccent,
+      child: Padding(
+        padding: EdgeInsets.only(top: _size.height / 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Stack(children: [
+              CircularText(
+                children: [
+                  TextItem(
+                    text: Text(
+                      "REPAIR WALL",
+                      style: TextStyle(
+                        fontSize: 28,
+                        color: Colors.blue,
+                        fontFamily: "Frijole",
+                      ),
+                    ),
+                    space: 20,
+                    startAngle: -90,
+                    startAngleAlignment: StartAngleAlignment.center,
+                    direction: CircularTextDirection.clockwise,
+                  ),
+                ],
+                radius: 150,
+                position: CircularTextPosition.inside,
+                backgroundPaint: Paint()..color = Colors.orangeAccent,
+              ),
+              ..._buildWall(_size.width),
+              ..._buildCharacters(_size.width)
+            ]),
+            ..._buildButtons(_size),
+          ],
+        ),
+      ),
     );
+  }
+
+  List<Widget> _buildButtons(Size size) {
+    return [
+      Column(
+        children: <Widget>[
+          SizedBox(
+            width: size.width / 2,
+            height: 50,
+            child: BasicButton(
+              () {},
+              text: "Start",
+            ),
+          ),
+          SizedBox(
+            height: size.height / 20,
+          ),
+          SizedBox(
+            width: size.width / 2,
+            height: 50,
+            child: BasicButton(
+              () {},
+              text: "Hiscore",
+            ),
+          )
+        ],
+      )
+    ];
+  }
+
+  List<Widget> _buildCharacters(double width) {
+    return [
+      Positioned(
+        width: 150,
+        height: 150,
+        bottom: 0,
+        child: const FlareActor("assets/walking.flr",
+            alignment: Alignment.bottomLeft,
+            fit: BoxFit.contain,
+            animation: "walking"),
+      ),
+      Positioned(
+        bottom: 0,
+        left: width / 3,
+        width: 150,
+        height: 150,
+        child: const NimaActor("assets/Hulk.nima",
+            alignment: Alignment.center,
+            fit: BoxFit.contain,
+            animation: "Walk"),
+      ),
+    ];
+  }
+
+  List<Widget> _buildWall(double width) {
+    return [
+      Positioned(
+        bottom: 0,
+        width: width / 1.65,
+        height: 70,
+        child: imageStoneWallBlock,
+      ),
+      Positioned(
+        bottom: 70,
+        width: width / 1.65,
+        height: 70,
+        child: imageStoneWallBlock,
+      ),
+      Positioned(
+        bottom: 140,
+        width: width / 1.65,
+        height: 70,
+        child: imageStoneWallBlock,
+      ),
+    ];
   }
 }
